007bf7ef47f5c9e40a1b141baba0e58ebd12b48b
Zain Zafar <zainzafar67@gmail.com>
2024-06-12 21:43:37 +0500

    Revert "netflix clone - boilerplate - singnin signup page"
    This reverts commit fc004ffa7c853fb8f17fe0d957c27211fb80834d.

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const MOVIE_API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2I2NzIwZGI2NzUxOWI2MTZjMmQzMTE1NmE3YmI2NyIsInN1YiI6IjY2Njk4OWJhZTM0MzdjYzVlNWJlYmRjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MKvnWFETpOfKHW0Au_Ah7RJDZNegjB3bU8N3mBozrzQ",
-  },
-};
